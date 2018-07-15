b0ae1a4146698dfb116b2aacd99f5256bd31e43a
izen1231 <izen1231@gmail.com>
2018-07-16 00:54:41 +0900

    up

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>
+		숭실고등학교 해킹방어대회
+	</title>
+
+
+	<meta content="yes" name="apple-mobile-web-app-capable">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<link rel="canonical" href="soongsilctf.github.io/index.html">
+	<meta name="description" content="숭실고등학교 해킹방어대회 사이트 입니다.">
+
+	<!-- 웹표준 오픈 그래프 -->
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="숭실고등학교 CTF">
+	<meta property="og:description" content="숭실고등학교 해킹방어대회 사이트 입니다.">
+	
+
+	
+    </span>
+	<!-- google analytics -->
+	<script>
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function () {
+				(i[r].q = i[r].q || []).push(arguments)
+			}, i[r].l = 1 * new Date();
+			a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script',
+			'https://www.google-analytics.com/analytics.js', 'ga');
+
+		ga('create', 'UA-105835640-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+
+	<link href="favicon.png" rel="shortcut icon" type="image/png">
+
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- CSS -->
+
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,900' rel='stylesheet' type='text/css'>
+	<!-- Styles -->
+	<link href="css/loader.css" rel="stylesheet" type="text/css">
+	<link href="css/normalize.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	
+
+	<script src="js/jquery.js"></script>
+</head>
+<body>
+<div class="preloader">
+	<div class="loading">
+		<h2>
+			로딩중...
+		</h2>
+		<span class="progress"></span>
+	</div>
+</div>
+
+<div class="wrapper">
+	<ul class="scene unselectable" data-friction-x="0.1" data-friction-y="0.1" data-scalar-x="25" data-scalar-y="15"
+	    id="scene">
+		<li class="layer" data-depth="0.00">
+		</li>
+		<!-- BG -->
+
+		<li class="layer" data-depth="0.10">
+			<div class="background">
+			</div>
+		</li>
+
+		<!-- Hero -->
+
+		<li class="layer" data-depth="0.20">
+			<div class="title">
+				<h2>
+				    숭실고등학교 해킹방어대회날까지
+				</h2>
+				<span class="line"></span>
+			</div>
+		</li>
+
+		<li class="layer" data-depth="0.30">
+			<div class="hero">
+				<h1 id="countdown">
+					
+
+					2018년 9월 xx일 숭실고등학교 해킹방어대회날까지
+				</h1>
+
+			</div>
+		</li>
+		<!-- Flakes -->
+
+		<li class="layer" data-depth="0.40">
+			<div class="depth-1 flake1">
+				<img alt="flake" src="images/flakes/depth1/flakes1.png">
+			</div>
+
+			<div class="depth-1 flake2">
+				<img alt="flake" src="images/flakes/depth1/flakes2.png">
+			</div>
+
+			<div class="depth-1 flake3">
+				<img alt="flake" src="images/flakes/depth1/flakes3.png">
+			</div>
+
+			<div class="depth-1 flake4">
+				<img alt="flake" src="images/flakes/depth1/flakes4.png">
+			</div>
+		</li>
+
+		<li class="layer" data-depth="0.50">
+			<div class="depth-2 flake1">
+				<img alt="flake" src="images/flakes/depth2/flakes1.png">
+			</div>
+
+			<div class="depth-2 flake2">
+				<img alt="flake" src="images/flakes/depth2/flakes2.png">
+			</div>
+		</li>
+
+		<li class="layer" data-depth="0.60">
+			<div class="depth-3 flake1">
+				<img alt="flake" src="images/flakes/depth3/flakes1.png">
+			</div>
+
+			<div class="depth-3 flake2">
+				<img alt="flake" src="images/flakes/depth3/flakes2.png">
+			</div>
+
+			<div class="depth-3 flake3">
+				<img alt="flake" src="images/flakes/depth3/flakes3.png">
+			</div>
+
+			<div class="depth-3 flake4">
+				<img alt="flake" src="images/flakes/depth3/flakes4.png">
+			</div>
+		</li>
+
+		<li class="layer" data-depth="0.80">
+			<div class="depth-4">
+				<img alt="flake" src="images/flakes/depth4/flakes.png">
+			</div>
+		</li>
+
+		<li class="layer" data-depth="1.00">
+			<div class="depth-5">
+				<img alt="flake" src="images/flakes/depth5/flakes.png">
+			</div>
+		</li>
+		<!-- Contact -->
+
+		<li class="layer" data-depth="0.20">
+			<div class="contact">
+				<ul class="icons">
+					<li>
+						<a href="https://www.facebook.com/SHSCTF/"><i class="fa fa-facebook"></i></a>
+					</li>
+					<li>
+						<a href="https://github.com/SOOGSILPCL"><i class="fa fa-github"></i></a>
+					</li>
+				</ul>
+				<a class="mail" href="https://soongctf.com">2017 숭실고등학교 CTF</a>
+				<br>
+				<br>
+				<a class="mail" href="https://soongsil.net">질문하기</a>
+			</div>
+		</li>
+	</ul>
+</div>
+
+<!-- Javascript -->
+<script src="js/plugins.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
